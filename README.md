7b60f69d27d0cf0bb9837749e67b856cc5ea119d
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-01-19 13:57:38 -0600

    draw lines

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Maze Solver
 
 Boot.dev project
+
+Note: tkinter had problems when using python 3.10. Python 3.9 worked okay.
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Line, Point
 
+def main():
+    win = Window(800, 600)
+    my_line = Line(Point(50, 500), Point(30, 40))
+    win.draw_line(my_line)
+    my_line_2 = Line(Point(3, 4), Point(50, 300))
+    win.draw_line(my_line_2)
+    win.wait_for_close()
+
+main()
